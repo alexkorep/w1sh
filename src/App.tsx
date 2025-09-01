c5f97e40b53ea2233582b25edc49e6c6fd34ed82
Alexey Korepanov <alexkorep@gmail.com>
2025-09-01 23:06:58 +0200

    feat: add game state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import './App.css'
 import Chat from './pages/Chat'
+import Console from './pages/Console'
+import useGameState from './hooks/useGameState'
 
 function App() {
-  return <Chat />
+  const { page } = useGameState()
+
+  switch (page) {
+    case 'console':
+      return <Console />
+    case 'chat':
+    default:
+      return <Chat />
+  }
 }
 
 export default App
