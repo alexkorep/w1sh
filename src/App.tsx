9e6260bb91e85dd1d085ab5f0d44a4d7cc72871f
Alexey Korepanov <alexkorep@gmail.com>
2025-09-02 08:09:35 +0200

    Replace static ad HTML with React component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import "./App.css";
+import AdPage from "./pages/AdPage";
 import Chat from "./pages/Chat";
 import Console from "./pages/Console";
 import useGameState from "./hooks/useGameState";
 
 function App() {
-  const { page, onChatComplete } = useGameState();
+  const { page, setPage, onChatComplete } = useGameState();
 
   return (
     <div className="app-container">
-      {page === "console" ? <Console /> : <Chat onComplete={onChatComplete} />}
+      {page === "ad" ? (
+        <AdPage onMessageSeller={() => setPage("chat")} />
+      ) : page === "chat" ? (
+        <Chat onComplete={onChatComplete} />
+      ) : (
+        <Console />
+      )}
     </div>
   );
 }
