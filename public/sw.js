d3b5f5faabc91b83506f2cff72711357baf1dfab
Alexey Korepanov <alexey.korepanov@clickhouse.com>
2025-09-06 17:05:56 +0200

    feat: update service worker to manage cache versioning

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,38 @@
-const CACHE_NAME = 'w1sh-cache-v1';
-const BASE_PATH = self.location.pathname.replace(/sw\.js$/, '');
-const URLS_TO_CACHE = [BASE_PATH];
+const CACHE_NAME = "w1sh-cache-v1";
+const BASE_PATH = self.location.pathname.replace(/sw\.js$/, "");
+const URLS_TO_CACHE = [
+  BASE_PATH,
+  `${BASE_PATH}manifest.webmanifest`,
+  `${BASE_PATH}images/w1sh-icon.png`,
+];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(URLS_TO_CACHE);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
